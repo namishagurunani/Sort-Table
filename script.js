a5724e78f883fee421dfce7fc1861ba19ba48082
namishagurunani <126158413+namishagurunani@users.noreply.github.com>
2023-10-16 16:11:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
     const customerTable = document.getElementById("customerTable");
     const sortButton = document.getElementById("sortButton");
+    let ascendingOrder = true;
 
     sortButton.addEventListener("click", function() {
         const rows = Array.from(customerTable.rows).slice(1); // Skip the header row
         rows.sort((a, b) => {
             const nameA = a.cells[0].textContent.toLowerCase();
             const nameB = b.cells[0].textContent.toLowerCase();
-            return nameA.localeCompare(nameB);
+
+            if (ascendingOrder) {
+                return nameA.localeCompare(nameB);
+            } else {
+                return nameB.localeCompare(nameA);
+            }
         });
 
-        // Clear the table
         while (customerTable.rows.length > 1) {
             customerTable.deleteRow(1);
         }
 
-        // Rebuild the table with sorted data
         rows.forEach(row => customerTable.tBodies[0].appendChild(row));
+
+        ascendingOrder = !ascendingOrder;
+        sortButton.textContent = ascendingOrder ? "Sort Ascending" : "Sort Descending";
     });
 });
